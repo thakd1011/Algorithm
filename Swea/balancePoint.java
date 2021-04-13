4718cd3eb1282195e3a2ecf912f8bdd19d62fd16
hope_mac <thakd1011@naver.com>
2021-04-13 14:55:31 +0900

    couldn't found yet why third testcase is wrong answer

---
+++ Swea/balancePoint.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+class balancePoint
+{
+    static double[] balancedPoint;
+    static int[] xCoordination;
+    static int[] mass;
+    static int N;
+    static double err;
+
+    static double mPow(double x, int n) {
+        double num = x;
+        for(int i = 0; i < n - 1; i++) {
+            x = x * num;
+        }
+        return x;
+    }
+
+    static int isPossible(int idx, double point) {
+        double leftForce = 0;
+        double rightForce = 0;
+        double tempForce;
+        double dist;
+        // left에 있는건 0부터 idx까지 해당
+        // right에 있는건 idx + 1부터 N-1까지 해당
+        // F = m1*m2/(d*d)
+//        System.out.println("point = " + point);
+        for(int i = 0; i < N; i++) {
+            dist = point - xCoordination[i];
+            tempForce = (double)(mass[i] / mPow(dist, 2));
+//            System.out.println("dist = " + mPow(dist, 2));
+//            System.out.println("temp F = " + tempForce);
+            if(i <= idx) {
+                leftForce += tempForce;
+            }
+            else {
+                rightForce += tempForce;
+            }
+        }
+//        System.out.println("left = " + leftForce + ", right = " + rightForce);
+        double pivot = rightForce - leftForce;
+        if(pivot < err && pivot > -err) return 0;
+        else if(pivot < 0) return -1;
+        else return 1;
+    }
+
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+            N = sc.nextInt();
+            balancedPoint = new double[N - 1];
+            xCoordination = new int[N];
+            mass = new int[N];
+
+            for(int i = 0; i < N; i++) {
+                xCoordination[i] = sc.nextInt();
+            }
+            for(int i = 0; i < N; i++) {
+                mass[i] = sc.nextInt();
+            }
+
+            double start, end;
+            double middle;
+            err = 1 / mPow(10, 13);
+            for(int i = 0; i < N - 1; i++) {
+                start = xCoordination[i];
+                end = xCoordination[i + 1];
+                while(end - start > err) {
+                    middle = (double)(start + end) / 2;
+                    int possibleVal = isPossible(i, middle);
+                    if(possibleVal == 0) {
+                        start = middle;
+                        break;
+                    }
+                    else if(possibleVal == 1){
+                        // move onto left side;
+                        end = middle - err;
+                    }
+                    else {
+                        start = middle + err;
+                    }
+                }
+                balancedPoint[i] = start;
+            }
+
+            System.out.print("#" + test_case);
+            for(int i = 0; i < N - 1; i++) {
+                System.out.print(" " + String.format("%.10f", balancedPoint[i]));
+            }
+            System.out.println();
+        }
+    }
+}
