71169f4221d50fad584d4a16cc1fd13538824158
hope_mac <thakd1011@naver.com>
2021-04-22 22:58:45 +0900

    Sort the task list with end-time at first and then consider if it includes i-th task or not.

--- Swea/freelancer.java
+++ Swea/freelancer.java
@@ -1,24 +1,43 @@
 import java.util.Scanner;
-public class freelancer {
+public class Solution {
     static int N, M;
-    static class task {
-        int start, end, cost;
-        task(int s, int e, int c) {
-            start = s;
-            end = e;
-            cost = c;
-        }
-    }
     static task[] taskList;
     static long ans = 0;
-    static void findMaxPrice(int tempTime, long tempCost) {
-        if(ans < tempCost) ans = tempCost;
 
-        for(int i = 0; i < N; i++) {
-            if(taskList[i].start > tempTime) {
-                findMaxPrice(taskList[i].end, tempCost + taskList[i].cost);
+    static void merge(int left, int mid, int right) {
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        task[] copiedTask = new task[N + 1];
+        while(i <= mid && j <= right) {
+            if(taskList[i].end < taskList[j].end) {
+                copiedTask[k++] = taskList[i++];
+            }
+            else {
+                copiedTask[k++] = taskList[j++];
             }
         }
+        while(i <= mid) {
+            copiedTask[k++] = taskList[i++];
+        }
+        while(j <= right) {
+            copiedTask[k++] = taskList[j++];
+        }
+        for(int m = left; m <= right; m++) {
+            taskList[m] = copiedTask[m];
+        }
+    }
+
+    static void mergeSort(int left, int right) {
+        if(left >= right) return;
+        int mid = (left + right) / 2;
+        mergeSort(left, mid);
+        mergeSort(mid + 1, right);
+        merge(left, mid, right);
+    }
+    static long maxCost(long a, long b) {
+        if( a > b) return a;
+        else return b;
     }
 
     public static void main(String args[]) {
@@ -27,18 +46,38 @@ public class freelancer {
         for(int tc = 1; tc <= T; tc++) {
             N = sc.nextInt();
             M = sc.nextInt();
-            taskList = new task[N];
+            taskList = new task[N + 1];
+            long[] dp = new long[N + 1];
             int s, e, c;
-            for(int i = 0; i < N; i++) {
+            for(int i = 1; i <= N; i++) {
                 s = sc.nextInt();
                 e = sc.nextInt();
                 c = sc.nextInt();
                 taskList[i] = new task(s, e, c);
             }
+            mergeSort(1, N);
+            
+            for(int i = 1; i <= N; i++) {
+                int prevIdx = 0;
+                for(int j = i - 1; j > 0; j--) {
+                    if(taskList[i].start > taskList[j].end) {
+                        prevIdx = j;
+                        break;
+                    }
+                }
+                dp[i] = maxCost(dp[i - 1], (dp[prevIdx] + taskList[i].cost));
+            }
+            System.out.println("#" + tc + " " + dp[N]);
+        }
+    }
 
-            findMaxPrice(0, 0);
-            System.out.println("#" + tc + " " + ans);
+
+    static class task {
+        int start, end, cost;
+        task(int s, int e, int c) {
+            start = s;
+            end = e;
+            cost = c;
         }
     }
 }
-
