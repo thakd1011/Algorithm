e31eeac5107c1907168fe8760fe9c330d9cf14d6
hope_mac <thakd1011@naver.com>
2021-04-26 21:07:11 +0900

    KMP algorithm applies to this problem to reduce time complexity O(N^2) to O(N+M)

--- Swea/CountingWord.java
+++ Swea/CountingWord.java
@@ -1,33 +1,49 @@
 class UserSolution {
-    final static double MAX_HASH_SIZE = 1e+9 + 9;
-    static double[] hashTb;
-    static int[] wordCnt;
-
-    static double mPow(int a, int b) {
-        double ret = 1;
-        for(int i = 0; i < b; i++) {
-            ret = ret * a;
-        }
-        return ret;
-    }
+    static int[] pi;
 
     public static int FindString(int N, String A, int M, String B) {
         int answer = 0;
-        double sHash = 0;
-        double bHash = 0;
-        double pow = 1;
+        // A = origin string, B = shorter string what we want to find in A.
+        pi = new int[100001];
 
-        for(int i = M - 1; i >= 0; i--) {
-            sHash += (double)A.charAt(i) * pow;
-            bHash += (double)B.charAt(i) * pow;
-            if(i > 0 ) pow *= 2;
+        // KMP Algorithm for making pi array.
+        int start = 1, matchedCnt = 0;
+        while(start + matchedCnt < M) {
+            if(B.charAt(start + matchedCnt) == B.charAt(matchedCnt)) {
+                matchedCnt++;
+                pi[start + matchedCnt - 1] = matchedCnt;
+            }
+            else {
+                if(matchedCnt == 0) {
+                    start++;
+                }
+                else {
+                    start += matchedCnt - pi[matchedCnt - 1];
+                    matchedCnt = pi[matchedCnt - 1];
+                }
+            }
         }
-
-        if(sHash == bHash) answer++;
-
-        for(int i = 1; i <= N - M; i++) {
-            sHash = 2 * (sHash - (double)A.charAt(i - 1) * pow) + (int)A.charAt(i + M - 1);
-            if(sHash == bHash) answer++;
+        // KMP Algorithm for searching B in A.
+        int begin = 0;
+        matchedCnt = 0;
+        while(begin <= N - M) {
+            // when the specific character coincides with from origin string.
+            if(matchedCnt < M && A.charAt(begin + matchedCnt) == B.charAt(matchedCnt)) {
+                matchedCnt++;
+                if(matchedCnt == M) {
+                    answer++;
+                }
+            }
+            // when the value of character doesn't have the same characters.
+            else {
+                if(matchedCnt == 0) {
+                    begin++;
+                }
+                else {
+                    begin += matchedCnt - pi[matchedCnt - 1];
+                    matchedCnt = pi[matchedCnt - 1];
+                }
+            }
         }
 
         return answer;
